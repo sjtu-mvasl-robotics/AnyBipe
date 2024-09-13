4271ac44d9cc6aceb7a2578c212382ba056a408d
securityCFS <710244487@qq.com>
2024-09-13 13:36:44 +0800

    edit dockerfile and update config

--- gyms/pointfoot-legged-gym/legged_gym/envs/pointfoot/mixed_terrain/pointfoot_rough_config.py
+++ gyms/pointfoot-legged-gym/legged_gym/envs/pointfoot/mixed_terrain/pointfoot_rough_config.py
@@ -41,7 +41,7 @@ class PointFootRoughCfg(BaseConfig):
         curriculum = False
         max_curriculum = 1.
         num_commands = 4  # default: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
-        resampling_time = 50.  # time before command are changed[s]
+        resampling_time = 10.  # time before command are changed[s]
         heading_command = True  # if true: compute ang vel command from heading error
 
         class ranges:
